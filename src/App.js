b7f04db202ba8c2fbccac1147ed86ea40c9334bc
csuyeon190 <160815064+sychoisimsys@users.noreply.github.com>
2024-03-20 14:41:43 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Fragment, useEffect, } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function App() {
+
+
+  const { unityProvider,loadingProgression, isLoaded, sendMessage, addEventListener, removeEventListener } =
+  useUnityContext({
+    loaderUrl: "Build/samsung_cnt_build8.loader.js",
+    dataUrl: "Build/samsung_cnt_build8.data",
+    frameworkUrl: "Build/samsung_cnt_build8.framework.js",
+    codeUrl: "Build/samsung_cnt_build8.wasm",
+  });
+  const handleGameOver = () =>{
+    console.log("react webgl test!!!!!!!!");
+
+
+
+
+
+
+  }
+  useEffect(() => {
+    addEventListener("ReactCall", handleGameOver);
+    return () => {
+      removeEventListener("ReactCall", handleGameOver);
+    };
+  }, [addEventListener, removeEventListener, handleGameOver]);
+
+
+
+
+  function handleClickSpawnEnemies() {
+    sendMessage("GameController", "SpawnEnemies", 100);
+  }
+  return (
+    <Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <button>test</button>
+        {!isLoaded && (
+          <div>
+          <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+          <div style={{ width: '100%', backgroundColor: '#eee' }}>
+              <div style={{ height: '20px', width: `${loadingProgression * 100}%`, backgroundColor: 'green' }}></div>
+            </div>
+          </div>
+      )}
+          <Unity style= {{  
+            width: '100%',
+            height:'100%',
+            justifyContent:'center',
+            alignItems:'center',
+            alignSelf:'center',
+            visibility: isLoaded ? "visible" : "hidden" 
+            }}    
+      unityProvider={unityProvider} />
+
+      <button onClick={handleClickSpawnEnemies}> test2</button>
+
+      </header>
+    </div>
+    </Fragment>
+  );
+}
+
+export default App;
